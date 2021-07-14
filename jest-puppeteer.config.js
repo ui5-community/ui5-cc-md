7d1c69cf6840c8a8fb6c9fa41fbe0553f45847c6
Volker Buzek <volker.buzek@js-soft.com>
2021-07-14 12:22:52 +0200

    ci: try ci-only ui5.yaml

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     server: {
         launchTimeout: 20000,
-        command: "yarn test:manual",
+        command: `yarn test:manual --config ${__dirname}/test/ui5-app/ui5-ci-only.yaml`,
         port: 8080,
         usedPortAction: "kill"
     }
